5be62a18e80a247d7304656523c64f8888301d04
Kars <karsschaapherder@outlook.com>
2022-07-04 18:39:38 +0200

    homiekars.nl

--- js/js-localStorage.js
+++ js/js-localStorage.js
@@ -2,6 +2,5 @@ document.addEventListener("keydown", function (zEvent) {
     if (zEvent.shiftKey && zEvent.key === "C") {
         localStorage.clear();
         location.reload();
-        console.log("tüm yerel depolamalar silindi")
     }
 });
--- js/js-modals.js
+++ js/js-modals.js
@@ -2,11 +2,8 @@ document.addEventListener("keydown", function (zEvent) {
     const modal = document.getElementById("commandPalette");
     if (zEvent.shiftKey && zEvent.key === "K") {
         if(modal.classList.contains("show") === true) {
-            $('#commandPalette').modal('hide');
-            console.log("modal kapatıldı")
         } else {
             $("#commandPalette").modal();
-            console.log("modal açıldı")
         }
     }
 });
@@ -15,5 +12,4 @@ const btn_c = document.querySelector('.command-palette-btn');
 
 btn_c.addEventListener('click', () => {
     $("#commandPalette").modal();
-    console.log("modal açıldı")
 });
